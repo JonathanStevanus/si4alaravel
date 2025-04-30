1aee684d331c0af2e35a09a3cebe35ddaf8cbf7b
JonathanStevanus <jonathanstv1343@gmail.com>
2025-04-30 15:08:08 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+route::get('/profil',function(){
+    return view('profil');
+});
+
+Route::resource('/fakultas',FakultasController::class);
